7a5f604abbf9f1cb3ca111d162dbaf94421f1ef5
Przemyslaw Biecek <przemyslaw.biecek@gmail.com>
2013-11-16 23:47:00 +0100

    wykres akomodacji z wiekiem

--- Percepcja/akomodacja.R
+++ Percepcja/akomodacja.R
@@ -8,12 +8,15 @@ y <- c(20,16,# 4 8
        1.5,1.2,#56
        1)
 
+pdf("akomodacja.pdf",7,8)
+
 plot(x,y, las=1, ylim=c(0,21),xlim=c(0,62), xaxs="i",yaxs="i",
      xlab="wiek", ylab="typowa zdolność skupiania (D)",
      type="o", pch=19,bty="n")
 abline(h=seq(0,20,2.5), lty=3, col="#00000077")
 abline(v=seq(0,60,5), lty=3, col="#00000077")
 
+dev.off()
 
 
 # zdrodlo:
