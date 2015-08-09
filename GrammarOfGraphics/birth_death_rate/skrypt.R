27755efc682e7d38dce683ffc0a5c7aacda518f4
pbiecek <przemyslaw.biecek@gmail.com>
2015-08-09 10:17:28 +0200

    dodane density 2d

--- GrammarOfGraphics/birth_death_rate/skrypt.R
+++ GrammarOfGraphics/birth_death_rate/skrypt.R
@@ -10,14 +10,14 @@ rates[,3] <- as.numeric(as.character(rates[,3]))
 colnames(rates) <- c("country","Birth_rate","Death_rate")
 
 library(ggplot2)
+library(MASS)
 
-pl <- ggplot(rates, aes(x=Birth_rate, y=Death_rate, 
-                  color=country=="Poland",
-                  size=country=="Poland")) +
+pl <- ggplot(rates, aes(x=Birth_rate, y=Death_rate)) +
+  geom_density2d(h=c(10,10), color="grey") +
   geom_point() + coord_fixed() +
   geom_abline(xintercept=0,slope=1) + 
-  scale_color_manual(values=c("black","red"))+
-  theme(legend.position="none")
+  geom_point(data=rates[136,], color="red", size=6) + 
+  theme_bw() 
 
 pl
 
