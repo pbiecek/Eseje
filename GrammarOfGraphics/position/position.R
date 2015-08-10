3e998eeedf09a491b18cc130f887d31664972921
pbiecek <przemyslaw.biecek@gmail.com>
2015-08-10 22:26:04 +0200

    rozne position_

--- GrammarOfGraphics/position/position.R
+++ GrammarOfGraphics/position/position.R
@@ -3,4 +3,17 @@ library(ggplot2)
 ggplot(iris, aes(x=Species, y=Sepal.Length)) +
   geom_point()
 
+ggplot(iris, aes(x=Species, y=Sepal.Length)) +
+  geom_point(position = position_dodge())
 
+ggplot(iris, aes(x=Species, y=Sepal.Length)) +
+  geom_point(position = position_stack(0.1, 0))
+
+ggplot(iris, aes(x=Species, y=Sepal.Length)) +
+  geom_point(position = position_fill())
+
+ggplot(iris, aes(x=Species, y=Sepal.Length)) +
+  geom_point(position = position_jitter(0.1, 0))
+
+ggplot(iris, aes(x=Species, fill=Species, y=Sepal.Length)) +
+  geom_point(position = position_jitterdodge(0.3, 0))
