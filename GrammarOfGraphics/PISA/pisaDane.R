f4cc8af19eac9719ef6137056a15da8790151074
pbiecek <przemyslaw.biecek@gmail.com>
2015-09-05 23:15:04 +0200

    pisa dane

--- GrammarOfGraphics/PISA/pisaDane.R
+++ GrammarOfGraphics/PISA/pisaDane.R
@@ -1,3 +1,18 @@
 library(PISA2012lite)
 library(dplyr)
 
+pisa <- student2012 %>%
+  select(CNT, ST04Q01, PV1MATH, PV1READ, W_FSTUWT) %>%
+  filter(CNT %in% c("Austria", "Belgium", "Bulgaria", "Switzerland",
+                    "Czech Republic", "Germany", "Denmark", "Spain", "Estonia",             
+                    "Finland", "France", "United Kingdom", "Greece", "Croatia", 
+                    "Hungary", "Ireland", "Iceland", "Italy", "Liechtenstein", "Lithuania",              
+                    "Luxembourg", "Latvia", "Netherlands", "Norway", "Poland", "Portugal",              
+                    "Romania", "Russian Federation", "Serbia", "Slovak Republic", "Slovenia",              
+                    "Sweden")) %>%
+  group_by(CNT, ST04Q01) %>%
+  summarize(math = weighted.mean(PV1MATH, W_FSTUWT),
+            read = weighted.mean(PV1READ, W_FSTUWT))
+
+
+ggplot(pisa, aes(math, read)) + geom_point()
