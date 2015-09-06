693c2a73a09f47d94c46f02907928bfb85a8f69e
pbiecek <przemyslaw.biecek@gmail.com>
2015-09-06 22:06:14 +0200

    mapy i jablka

--- GrammarOfGraphics/PISA/eurostat.R
+++ GrammarOfGraphics/PISA/eurostat.R
@@ -4,10 +4,9 @@ library(ggplot2)
 
 milk <- getEurostatRCV("apro_mk_pobta")
 
-EUcc <- c('AL', 'AD', 'AM', 'AT', 'BY', 'BE', 'BA', 'BG', 'CH', 'CY', 'CZ', 'DE',
-          'DK', 'EE', 'ES', 'FO', 'FI', 'FR', 'GB', 'GE', 'GI', 'GR', 'HU', 'HR',
-          'IE', 'IS', 'IT', 'LT', 'LU', 'LV', 'MC', 'MK', 'MT', 'NO', 'NL', 'PL',
-          'PT', 'RO', 'RU', 'SE', 'SI', 'SK', 'SM', 'TR', 'UA', 'VA')
+EUcc <- c(
+  'AT', 'BE', 'BG', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HU', 'IE', 'IT',
+  'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB', 'UK', 'EL')
 
 cowMilk <- milk %>%
   filter(milkitem == "PRO" & prodmilk == "MC001") %>%
@@ -16,16 +15,62 @@ cowMilk <- milk %>%
 ggplot(cowMilk, aes(x=time, y=value, fill=geo == "PL")) +
   geom_bar(stat="identity") + coord_flip()
 
-cowMilk2 <- cowMilk %>%
-  mutate(isPL = factor(geo == "PL")) %>%
-  group_by(isPL, time) %>%
-  summarise(total = sum(value, na.rm=TRUE))
 
-ggplot(cowMilk2, 
-       aes(x=time, y=total, fill=isPL)) +
-  geom_bar(stat="identity", position="fill") + 
-  coord_cartesian(ylim=c(0.85,1))
 
-sort(tapply(cowMilk$value, cowMilk$geo, sum, na.rm=TRUE))
 
-boxplot(cowMilk$value~cowMilk$geo)
+
+
+# apples
+library(ggplot2)
+library(grid)
+library(rworldmap)
+library(maptools) 
+gpclibPermit() 
+
+jablko <- getEurostatRCV("tag00036")
+
+jablko2014 <- milk %>%
+  filter(milkitem == "PRO" & prodmilk == "MC001" & time=="2014") %>%
+  filter(geo %in% EUcc)
+
+jablko2014 <- jablko %>% 
+  filter(time==2014, geo %in% EUcc)
+jablko2014$value[is.na(jablko2014$value)] = 0
+
+ggplot(jablkoEU, aes(x=time, y=value, group=geo, col=geo)) + 
+  geom_line()
+
+worldMap <- getMap()
+
+# Member States of the European Union
+indEU <- which(worldMap$ISO_A2 %in% EUcc)
+indEU <- seq_along(worldMap$ISO_A2)
+
+europeCoords <- lapply(indEU, function(i){
+  df <- data.frame(worldMap@polygons[[i]]@Polygons[[1]]@coords)
+  df$region = as.character(worldMap$NAME[i])
+  df$iso2 = as.character(worldMap$ISO_A2[i])
+  vv <- jablko2014[jablko2014$geo == as.character(worldMap$ISO_A2[i]),"value"]
+  if (as.character(worldMap$ISO_A2[i]) == "GB") {
+    vv <- jablko2014[jablko2014$geo == "UK","value"]
+  }
+  if (as.character(worldMap$ISO_A2[i]) == "GR") {
+    vv <- jablko2014[jablko2014$geo == "EL","value"]
+  }
+  if (length(vv) != 1) vv = NA
+  df$value = vv
+  colnames(df) <- list("long", "lat", "region", "iso2", "value")
+  return(df)
+})
+
+europeCoords <- do.call("rbind", europeCoords)
+
+pl1 <- ggplot() + geom_polygon(data = europeCoords, 
+                        aes(x = long, y = lat, group = factor(region), fill = value),
+                        colour = "black", size = 0.1) +
+  coord_map(xlim = c(-13, 35),  ylim = c(32, 71), projection = "mollweide")
+
+pl1
+
+ggsave(filename = "appleMap.pdf", pl1, width = 7, height = 7)
+
