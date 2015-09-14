9b6aa4d8dae84d624850088944969d08e3a8eed2
pbiecek <przemyslaw.biecek@gmail.com>
2015-09-14 21:35:13 +0200

    rozne punkty

--- GrammarOfGraphics/birth_death_rate/punkty.R
+++ GrammarOfGraphics/birth_death_rate/punkty.R
@@ -6,8 +6,31 @@ library(SmarterPoland)
 
 head(countries)
 
-# density
-ggplot(ratesSmall, aes(x=Birth_rate)) +
-  stat_density() + 
-  geom_rug() + 
-  theme_bw() 
+# dotplot
+plD <- ggplot(countries, aes(x = continent, y = birth.rate)) +
+  geom_dotplot(binaxis = "y", stackdir = "center", binwidth = 0.7) +
+  theme_bw()
+
+ggsave(plD, filename = "geomDotplot.pdf", width = 7, height = 7, useDingbats=FALSE)
+
+
+
+# dotplot
+plP <- ggplot(countries, aes(x = birth.rate, y =death.rate)) +
+  geom_point() +
+  theme_bw()
+
+ggsave(plP, filename = "geomPoint.pdf", width = 7, height = 7, useDingbats=FALSE)
+
+
+
+# jitter
+plJ <- ggplot(countries, aes(x = continent, y =birth.rate)) +
+  geom_jitter(position = position_jitter(width = .2)) +
+  theme_bw()
+
+ggsave(plJ, filename = "geomJitter.pdf", width = 7, height = 7, useDingbats=FALSE)
+
+
+
+
