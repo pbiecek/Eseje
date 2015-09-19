68269fdd117a61435bc0d3a741eafbb704a5a471
pbiecek <przemyslaw.biecek@gmail.com>
2015-09-19 12:56:27 +0200

    matura w kółko do kompozycji

--- GrammarOfGraphics/matura.R
+++ GrammarOfGraphics/matura.R
@@ -4,10 +4,24 @@
 library(ZPD)
 src = polacz()
 gh2011 = pobierz_wyniki_egzaminu(src, 'egzamin gimnazjalny', 'humanistyczna', 2011, TRUE)
-matura <- pobierz_wyniki_egzaminu(src, rodzajEgzaminu = "matura", czescEgzaminu = "j. polski podstawowa", 2014, TRUE)
+matura <- pobierz_wyniki_egzaminu(src, rodzajEgzaminu = "matura", 
+                                  czescEgzaminu = "j. polski podstawowa", 2014, TRUE)
 as.data.frame(pobierz_testy(src))
 
 matura <- collect(matura)
+
+matura <- list()
+
+for (i in 2010:2015) {
+  maturaPolski <- collect(pobierz_wyniki_egzaminu(src, rodzajEgzaminu = "matura", 
+                                          czescEgzaminu = "j. polski podstawowa", i, TRUE))
+  maturaMatematyka <- collect(pobierz_wyniki_egzaminu(src, rodzajEgzaminu = "matura", 
+                                          czescEgzaminu = "matematyka podstawowa", i, TRUE))
+  save(maturaPolski, maturaPolski, file=paste0("matura",i,".rda"))
+  matura[[i - 2009]] <- list(maturaPolski, maturaMatematyka)
+}
+
+
 
 suma <- rowSums(matura[,5:35], na.rm=TRUE)
 hist(suma,100)
