ca7d47727e21cb308cb8bcc44fa3bc0b6ec5f768
pbiecek <przemyslaw.biecek@gmail.com>
2015-09-27 10:52:59 +0200

    kody dla cumsum

--- GrammarOfGraphics/birth_death_rate/bar.R
+++ GrammarOfGraphics/birth_death_rate/bar.R
@@ -8,7 +8,8 @@ continents <-
   countries %>%
   group_by(continent) %>%
   summarise(birth.rate = mean(birth.rate, na.rm=TRUE),
-            death.rate = mean(death.rate, na.rm=TRUE))
+            death.rate = mean(death.rate, na.rm=TRUE),
+            population = sum(population))
   
 
 # bar
@@ -17,6 +18,19 @@ plB <- ggplot(continents, aes(x = continent, y = birth.rate)) +
   theme_bw()
 
 ggsave(plB, filename = "geomBar.pdf", width = 7, height = 7, useDingbats=FALSE)
+
+
+continents2 <- continents %>%
+  mutate(cum = cumsum(population)-population)
+
+barPP <- ggplot() +
+  geom_rect(data=continents2, aes(xmin=cum, ymax=0, xmax=cum+population, ymin=birth.rate, fill=continent)) +
+  geom_text(data=continents2, aes(label=continent, x=cum+population/2, y=birth.rate), vjust=-0.1) + 
+  theme_bw() + theme(legend.position="none") +
+  xlab("population") + ylab("birth.rate")
+
+ggsave(barPP, filename = "geomRectDW.pdf", width = 7, height = 7, useDingbats=FALSE)
+
 
 
 
