5c3ff3bd6e02774c995789d2a41427939fbe3fb0
pbiecek <przemyslaw.biecek@gmail.com>
2015-09-27 11:42:09 +0200

    nowe kody

--- GrammarOfGraphics/birth_death_rate/bar.R
+++ GrammarOfGraphics/birth_death_rate/bar.R
@@ -7,8 +7,8 @@ library(SmarterPoland)
 continents <- 
   countries %>%
   group_by(continent) %>%
-  summarise(birth.rate = mean(birth.rate, na.rm=TRUE),
-            death.rate = mean(death.rate, na.rm=TRUE),
+  summarise(birth.rate = weighted.mean(birth.rate, w = population, na.rm=TRUE),
+            death.rate = weighted.mean(death.rate, w = population, na.rm=TRUE),
             population = sum(population))
   
 
@@ -33,12 +33,27 @@ ggsave(barPP, filename = "geomRectDW.pdf", width = 7, height = 7, useDingbats=FA
 
 
 
+plRR3 <- ggplot() +
+  geom_rect(data=continents, aes(xmin = as.numeric(factor(continent))-0.1, 
+                                 ymin = 0, 
+                                 xmax = as.numeric(factor(continent))+0.2, 
+                                 ymax = birth.rate),
+            fill="green3") +
+  geom_rect(data=continents, aes(xmin = as.numeric(factor(continent))+0.21, 
+                                 ymin = birth.rate - death.rate, 
+                                 xmax = as.numeric(factor(continent))+0.51, 
+                                 ymax = birth.rate),
+            fill="red3") +
+  geom_text(data=continents, aes(x = as.numeric(factor(continent))+0.21, 
+                                 y = birth.rate + 1, 
+                                 label = continent)) + 
+  geom_hline(yintercept=0) + ylab("birth.rate - death.rate") + xlab("") +
+  geom_rug(data=continents, aes(x = as.numeric(factor(continent))+0.21, 
+                                y = birth.rate - death.rate), sides="l") +
+  theme_bw() + theme(axis.text.x = element_text(color="white"),
+                     axis.ticks.x = element_line(color="white"))
 
 
-# rect
-plR <- ggplot(continents, aes(xmin = death.rate, ymin = birth.rate, xmax = death.rate+1, ymax = birth.rate+1)) +
-  geom_rect() +
-  theme_bw()
 
-ggsave(plR, filename = "geomRect.pdf", width = 7, height = 7, useDingbats=FALSE)
+ggsave(plRR3, filename = "geomRect.pdf", width = 7, height = 7, useDingbats=FALSE)
 
