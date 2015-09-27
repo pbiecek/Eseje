650a33de8d52929ac85450e509ce7048229ca363
pbiecek <przemyslaw.biecek@gmail.com>
2015-09-27 14:26:05 +0200

    zrodla dla linii

--- GrammarOfGraphics/birth_death_rate/line.R
+++ GrammarOfGraphics/birth_death_rate/line.R
@@ -26,7 +26,8 @@ ggsave(plL, filename = "geomLine.pdf", width = 7, height = 7, useDingbats=FALSE)
 
 # smooth
 plS <- ggplot(countries, aes(x = birth.rate, y = death.rate)) +
-  geom_smooth() +
+  geom_point(color="white", alpha=0) +
+  geom_smooth(se=FALSE, size=3, color="black") +
   theme_bw()
 
 ggsave(plS, filename = "geomSmooth.pdf", width = 7, height = 7, useDingbats=FALSE)
@@ -59,6 +60,29 @@ ar2 <- ggplot() +
 
 ggsave(ar2, filename = "geomLine2.pdf", width = 7, height = 7, useDingbats=FALSE)
 
+
+
+ar3 <- ggplot() +
+  geom_segment(data=countries[countries$continent == "Europe",], aes(x = country, xend = country, 
+                                                                     y = birth.rate, yend=death.rate,
+                                                                     size=population),
+               arrow = arrow(length = unit(0.1,"cm"), type="closed")) +
+  theme_bw() + coord_flip() + theme(legend.position="none") + 
+  ylab("<---- more births                more deaths ---->") + xlab("") 
+
+ggsave(ar3, filename = "geomLine3.pdf", width = 7, height = 7, useDingbats=FALSE)
+
+
+ar4 <- ggplot() +
+  geom_segment(data=countries[countries$continent == "Europe",], aes(x = country, xend = country, 
+                                                                     y = birth.rate, yend=death.rate,
+                                                                     linetype=birth.rate > death.rate),
+               arrow = arrow(length = unit(0.1,"cm"), type="closed")) +
+  theme_bw() + coord_flip() + theme(legend.position="top") + 
+  ylab("<---- more births                more deaths ---->") + xlab("") +
+  scale_linetype_manual(values = c(1,2), labels=c("More births than deaths","More deaths than births"), name="")
+
+ggsave(ar4, filename = "geomLine4.pdf", width = 7, height = 7, useDingbats=FALSE)
 
 
 
