e2275af0df19c2445f52ace37a3659cc58609eb7
pbiecek <przemyslaw.biecek@gmail.com>
2015-09-29 08:32:08 +0200

    jednak nie pdf

--- GrammarOfGraphics/punkty2d.R
+++ GrammarOfGraphics/punkty2d.R
@@ -3,23 +3,33 @@ library(ggplot2)
 
 pl1 <- ggplot(maturaExam[maturaExam$rok == 2015,], 
        aes(podstawowy.matematyka, podstawowy.j.polski)) + 
-  geom_point() + theme_bw()
+  geom_point() + theme_bw()+
+  xlab("Punkty, matura z j. polskiego, poziom podstawowy") +
+  ylab("Punkty, matura z matematyki, poziom podstawowy") 
 
 
 pl2 <- ggplot(maturaExam[maturaExam$rok == 2015,], 
               aes(podstawowy.matematyka, podstawowy.j.polski)) + 
-  geom_jitter() + theme_bw()
+  geom_jitter() + theme_bw()+
+  xlab("Punkty, matura z j. polskiego, poziom podstawowy") +
+  ylab("Punkty, matura z matematyki, poziom podstawowy") 
 
 
 pl3 <- ggplot(maturaExam[maturaExam$rok == 2015,], 
               aes(podstawowy.matematyka, podstawowy.j.polski)) + 
-  geom_smooth(se=FALSE) + theme_bw() + ylim(0,74)
+  geom_smooth(se=FALSE) + theme_bw() + ylim(0,74)+
+  xlab("Punkty, matura z j. polskiego, poziom podstawowy") +
+  ylab("Punkty, matura z matematyki, poziom podstawowy") 
 
 
 pl4 <- ggplot(maturaExam[maturaExam$rok == 2015,], 
               aes(factor(podstawowy.matematyka), podstawowy.j.polski)) + 
-  geom_boxplot(coef=5) + theme_bw() + ylim(0,74)
+  geom_boxplot(coef=5) + theme_bw() + ylim(0,74) +
+  xlab("Punkty, matura z j. polskiego, poziom podstawowy") +
+  ylab("Punkty, matura z matematyki, poziom podstawowy") 
 
 
-
+ggsave(pl1, filename = "geomMatura1.png", width = 7, height = 7)
+ggsave(pl2, filename = "geomMatura2.png", width = 7, height = 7)
+ggsave(pl4, filename = "geomMatura4.pdf", width = 7, height = 7, useDingbats=FALSE)
 
