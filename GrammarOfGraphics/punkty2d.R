2c48b017e6ffa2c4b1bf9955f27048948968e2c0
pbiecek <przemyslaw.biecek@gmail.com>
2015-09-29 09:46:32 +0200

    zrob to ladniej

--- GrammarOfGraphics/punkty2d.R
+++ GrammarOfGraphics/punkty2d.R
@@ -32,16 +32,18 @@ maturaExam2 <- maturaExam[maturaExam$rok == 2015,] %>%
   group_by(podstawowy.matematyka) %>%
   summarise(podstawowy.j.polski=median(podstawowy.j.polski))
 
-ggplot(maturaExam[maturaExam$rok == 2015,], 
+pl5 <- ggplot(maturaExam[maturaExam$rok == 2015,], 
        aes(factor(podstawowy.matematyka), podstawowy.j.polski)) + 
-  geom_boxplot(fill="red3",color="red3", coef=5) + 
-  geom_jitter(size=1, alpha=0.03) + theme_bw() + ylim(0,74) +
-  geom_line(data=maturaExam2, aes(factor(podstawowy.matematyka), podstawowy.j.polski, group=1), color="navyblue2") +
+  geom_boxplot(fill="grey80",color="grey80", coef=5) + 
+  geom_jitter(size=1, alpha=0.05) + theme_bw() + ylim(0,74) +
+  geom_line(data=maturaExam2, aes(factor(podstawowy.matematyka), podstawowy.j.polski, group=1), color="red3", size=3) +
   xlab("Punkty, matura z j. polskiego, poziom podstawowy") +
-  ylab("Punkty, matura z matematyki, poziom podstawowy") 
+  ylab("Punkty, matura z matematyki, poziom podstawowy") +
+  scale_x_discrete(labels=c("0", rep("",9), "10", rep("",9), "20", rep("",9), "30", rep("",9), "40", rep("",9), "50"))
 
 
 ggsave(pl1, filename = "geomMatura1.png", width = 7, height = 7)
 ggsave(pl2, filename = "geomMatura2.png", width = 7, height = 7)
 ggsave(pl4, filename = "geomMatura4.pdf", width = 7, height = 7, useDingbats=FALSE)
+ggsave(pl5, filename = "geomMatura5.png", width = 7, height = 7)
 
